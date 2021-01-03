777c377e0783b61d58bad4b595b2a8da281fa1ea
MarcusViniciusLagana <71972975+MarcusViniciusLagana@users.noreply.github.com>
2021-01-03 01:21:11 -0300

    Requisition implemented

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+//import OpenSquare from './mines';
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/OpenSquare/:id', (req, res) => {
+  // index, squaresValues, squaresCSS, rows, columns
+  const index = +req.params.id;
+  let squaresValues = req.body.squaresValues;
+  let squaresCSS = req.body.squaresCSS;
+  const rows = req.body.rows;
+  const columns = req.body.columns;
+  const win = req.body.win;
+
+  res.send([index, squaresValues, squaresCSS, rows, columns, win]);
 });
 
 app.listen(port, () => {
---
+++ mines.js
@@ -0,0 +1,74 @@
+function CountMines (index, rowsNumber, columnsNumber) {
+    const cssClasses = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+    // index = row * columnsNumber + column
+    // index/columnsNumber = row (quotient) + column/columnsNumber (remainder)
+    const rowInit = Math.floor(index / columnsNumber);
+    const columnInit = index % columnsNumber;
+    let positions = [];
+
+    for (let row = rowInit - 1; row < rowInit + 2; row++) {
+        if (row < 0 || row > rowsNumber - 1) continue;
+        for (let column = columnInit -1; column < columnInit + 2; column++) {
+            if (row === rowInit && column === columnInit) continue;
+            if (column < 0 || column > columnsNumber - 1) continue;
+            positions.push(row * columnsNumber + column);
+        }
+    }
+
+    // Count mines in adjacent squares
+    let mines = 0;
+    for (const position of positions) if (minesPositions.includes(position)) mines++;
+
+    // return the number of mines, the updated cssClass and the valid positions around index
+    return([mines === 0 ? '' : mines, 'clicked ' + cssClasses[mines], positions]);
+}
+
+function OpenAllSquares (squaresValues, squaresCSS, rows, columns, win=false) {
+    for (let index = 0, length = squaresValues.length; index < length; index++) {
+        if (minesPositions.includes(index)) {
+            squaresValues[index] = squaresCSS[index] === 'saved' || win ? '\u2713' : bombSymbol;
+            squaresCSS[index] = squaresCSS[index] === 'saved' || win ? 'saved-true' : 'clicked exploded';
+        }
+        if (squaresCSS[index] === 'saved') {
+            squaresValues[index] = '\u2717';
+            squaresCSS[index] = 'exploded';
+        }
+        if (!squaresCSS[index]) {
+            [squaresValues[index], squaresCSS[index]] = CountMines(index, rows, columns);
+        }
+    }
+    return;
+}
+
+export default function OpenSquare (index, squaresValues, squaresCSS, rows, columns, win=false) {
+
+    if (win) {
+        OpenAllSquares(squaresValues, squaresCSS, rows, columns, win);
+        return true;
+    }
+
+    if (minesPositions.includes(index)) {
+        OpenAllSquares(squaresValues, squaresCSS, rows, columns);
+        squaresCSS[index] = 'clicked';
+        return false;
+    }
+
+    let allPositions = [index];
+    let positions = [];
+    let i = 0;
+    
+    while (true) {
+        // if square was not clicked:
+        if (!squaresCSS[allPositions[i]]) {
+            // Count mines around the square, update value with the number of mines
+            // and squaresCSS with 'clicked ' + the number of mines as text
+            // positions keep the indexes of the squares around
+            [squaresValues[allPositions[i]], squaresCSS[allPositions[i]], positions] = CountMines(allPositions[i], rows, columns);
+            if (!squaresValues[allPositions[i]]) {
+                for (const pos of positions) if (!allPositions.includes(pos)) allPositions.push(pos);
+            }
+        }
+        if (i < allPositions.length - 1) i++;
+        else return true;
+    }
+}
